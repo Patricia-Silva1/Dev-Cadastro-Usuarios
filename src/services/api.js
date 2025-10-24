21af86500eb7c0ffee6296f3018ea28997d3905b
Patrícia Silva <ps397380@gmail.com>
2025-10-23 22:23:08 -0300

    Atualizar o api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cadastro-api-dr5y.onrender.com", // Backend hospedado no Render
+  baseURL: "https://cadastro-api-dr5y.onrender.com/usuarios", // Backend hospedado no Render
   headers: {
     "Content-Type": "application/json"
   }
